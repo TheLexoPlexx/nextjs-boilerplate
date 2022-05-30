f5a9b11ea83d3dae7c8aa5319f73e21992a78ff2
Alex <17780186+TheLexoPlexx@users.noreply.github.com>
2022-05-30 09:08:39 +0200

    Boilerplated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import Navbar from '../components/navbar'
-import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>CryptoBot Dashboard</title>
-      </Head>
-
-      <Navbar />
-
-      <div className={styles.main}>
-        <h1>Login</h1>
-        <h3>Hier steht Text.</h3>
-        <Link href="/login">
-          <a>Link</a>
-        </Link>
-      </div>
-    </div>
+    <></>
   )
 }
 
 export default Home
-
-// https://nextjs.org/examples
-// https://www.freecodecamp.org/news/dynamic-navigation-in-nextjs/
