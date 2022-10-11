7f684e30e6b95b06425601511ef77d5c229dbf47
Alex <17780186+TheLexoPlexx@users.noreply.github.com>
2022-10-11 19:05:06 +0200

    Fixed Mixins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "styles/mixins.scss";`
+  }
 }
