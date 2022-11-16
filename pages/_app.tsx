3d414eac83cd4fa47659bd40fecf22031960c7da
Alex <17780186+TheLexoPlexx@users.noreply.github.com>
2022-11-16 12:14:11 +0100

    Header-Font funktioniert nicht.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,28 @@
+import type { AppProps } from 'next/app';
 
-import '../styles/containers.css'
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '../styles/containers.css'; //entfernen
+import '../styles/globals.scss';
+
+import { Raleway, Roboto_Slab } from '@next/font/google';
+
+const raleway = Raleway({
+  fallback: [ "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif" ],
+  subsets: [ "latin" ],
+});
+
+const roboto_slab = Roboto_Slab({
+  fallback: [ "serif" ],
+  subsets: [ "latin"],
+  variable: "--font-hx"
+});
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const dummy_hx = roboto_slab.className
+  return (
+    <main className={raleway.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default App
