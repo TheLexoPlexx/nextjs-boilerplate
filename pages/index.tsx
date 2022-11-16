4ee5b9195f42d91c146c71bf555ff80cae600839
Alex <17780186+TheLexoPlexx@users.noreply.github.com>
2022-11-16 08:58:03 +0100

    Elements-Knopf zentriert

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import { CSSProperties } from 'react'
 
 const Home: NextPage = () => {
+  let bodyStyle: CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100vw",
+    height: "100vh"
+  }
+  
   return (
-    <>
-      <Link href={"/elements"}>Elements</Link>
-    </>
+    <div style={bodyStyle}>
+      <Link href={"/elements"}><h1>Elements</h1></Link>
+    </div>
   )
 }
 
